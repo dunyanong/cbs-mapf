720abd515ef18cdd476b35f76793363cd41298b8
Ong Dun Yan <ongdunyan@Ongs-MacBook-Air.local>
2025-07-02 21:14:55 +0800

    setup SFML

---
+++ animation/dikjstra_animation.cpp
@@ -0,0 +1,32 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
+
+    // Load a sprite to display
+    const sf::Texture texture("messi.jpeg");
+    sf::Sprite sprite(texture);
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        while (const std::optional event = window.pollEvent())
+        {
+            // Close window: exit
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        // Clear screen
+        window.clear();
+
+        // Draw the sprite
+        window.draw(sprite);
+
+        // Update the window
+        window.display();
+    }
+}
